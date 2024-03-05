642adcbeedc68fe1a38287cbd5a9e566aeec8532
Krupal Patel <krupal9102001@gmail.com>
2024-03-05 18:50:33 +0530

    Added some functionality like validation and more

--- JS/app.js
+++ JS/app.js
@@ -7,25 +7,48 @@ showNotes();
 let addBtn = document.getElementById("addBtn");
 addBtn.addEventListener('click', function (e) {
 
-
-    let addTxt = document.getElementById('addTxt');
+    e.preventDefault()
+    let addTxt = document.getElementById('addTxt').value;
     let notes = localStorage.getItem('notes');
-    let addTtl = document.getElementById('addTtl')
+    let addTtl = document.getElementById('addTtl').value;
     if (notes == null) {
         notesObj = [];
     }
     else {
         notesObj = JSON.parse(notes);
     }
-    notesObj.push({txt:addTxt.value,
-                   title:addTtl.value
-    });
+// console.log(addTxt.length,addTtl.length)
+
+    if ((addTxt.length > 2 ) && (addTtl.length > 2)) {
+        notesObj.push({
+            txt: addTxt,
+            title: addTtl
+        });
+         if (display.validate(notes)) {
+             display.show('success', '   Your book has been successfully added.');
+             display.clear()
+         } 
+    } 
+    else {
+        display.show('danger', '   Sorry! You cannot fill all details.');
+        display.clear()
+
+        
+
+     }
+
+
+
     // console.log({txt:addTxt.value,title:addTtl.value},"working")
     localStorage.setItem('notes', JSON.stringify(notesObj));
+    addTtl.value = ""
     addTxt.value = "";
     // console.log(notesObj)
     showNotes();
 })
+
+
+
 // Function to show Element  from Local Storage
 function showNotes() {
     let notes = localStorage.getItem('notes');
@@ -56,9 +79,10 @@ function showNotes() {
     let notesElm = document.getElementById('notes');
     if (notesObj.length != 0) {
         notesElm.innerHTML = html;
+
     }
     else {
-        notesElm.innerHTML = `Nothing to show! USe "Add a Note" section above to add notes.`
+        notesElm.innerHTML = `Nothing to show! Use "Add a Note" section above to add notes.`
     }
 
 
@@ -67,7 +91,7 @@ function showNotes() {
 
 // Function to Delete Note
 
-function deleteNote(index){
+function deleteNote(index) {
     console.log("i am deleting", index);
     let notes = localStorage.getItem('notes');
     if (notes == null) {
@@ -76,36 +100,92 @@ function deleteNote(index){
     else {
         notesObj = JSON.parse(notes);
     }
-    
-   notesObj.splice(index, 1);
-   localStorage.setItem('notes', JSON.stringify(notesObj));
 
-   showNotes()
+    notesObj.splice(index, 1);
+    localStorage.setItem('notes', JSON.stringify(notesObj));
+
+    showNotes()
 }
 
 // Search Button
 let search = document.getElementById('searchTxt');
 
-search.addEventListener("input", function(){
+search.addEventListener("input", function () {
 
     let inputVal = search.value.toLowerCase();
 
     // console.log("input event fired" , inputVal);
     let noteCards = document.getElementsByClassName('noteCard');
-    Array.from(noteCards).forEach(function(element){
+    Array.from(noteCards).forEach(function (element) {
         let cardTxt = element.getElementsByTagName("p")[0].innerText;
-        if(cardTxt.toLowerCase().includes(inputVal)){
+        if (cardTxt.toLowerCase().includes(inputVal)) {
             element.style.display = "block";
         }
-        else{
+        else {
             element.style.display = "none";
         }
-        
+
         // console.log(cardTxt);
     })
 
 
 
-    
+
 
 })
+
+
+//Display Constructor
+
+function Display() {
+
+}
+function Notes(addTtl, addTxt) {
+    this.addTtl = addTtl,
+        this.addTxt = addTxt
+}
+
+
+
+
+// Implement  the validate Function
+
+Display.prototype.validate = function (notes) {
+    if (addTtl.length < 2 || addTxt.length < 3) {
+        return false;
+
+    }
+    else {
+        return true;
+
+    }
+};
+Display.prototype.show = function (type, message) {
+
+    let msg = document.getElementById('msg');
+    if (type === 'success') {
+        boldTXt = "Success"
+    }
+    else {
+        boldTXt = "Error"
+
+    }
+    msg.innerHTML = ` 
+    <div class="alert alert-${type} alert-dismissible fade show" role="alert">
+  <strong>${boldTXt}:</strong>    ${message}
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>`
+
+    setTimeout(function () {
+        msg.innerHTML = ``;
+    }, 4000)
+
+}
+
+let display = new Display();
+
+Display.prototype.clear = function () {
+    let libraryForm = document.getElementById('libraryForm');
+    libraryForm.reset();
+};
+
